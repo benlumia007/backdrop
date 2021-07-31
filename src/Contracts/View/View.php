49dfe6641d5e427bacee4ef4eb9780cb82826a27
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-07-31 10:47:16 -0700

    let's add the engin/view contracts

---
+++ src/Contracts/View/View.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Backdrop Core ( Contracts\View\View.php )
+ *
+ * @package   Backdrop Core
+ * @copyright Copyright (C) 2019-2021. Benjamin Lu
+ * @license   GNU General PUblic License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author    Benjamin Lu ( https://getbenonit.com )
+ */
+
+/**
+ * Define namespace
+ */
+namespace Benlumia007\Backdrop\Contracts\View;
+use Benlumia007\Backdrop\Contracts\Renderable;
+use Benlumia007\Backdrop\Contracts\Displayable;
+
+/**
+ * View interface.
+ *
+ * @since  2.0.0
+ * @access public
+ */
+interface View extends Renderable, Displayable {
+
+	/**
+	 * Returns the array of slugs.
+	 *
+	 * @since  2.0.0
+	 * @access public
+	 * @return array
+	 */
+	public function slugs();
+
+	/**
+	 * Returns the absolute path to the template file.
+	 *
+	 * @since  2.0.0
+	 * @access public
+	 * @return string
+	 */
+	public function template();
+}
