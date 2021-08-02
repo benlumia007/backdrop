df3196f63a6af8f4213620dc2c06356fdb4b233e
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-08-01 20:22:10 -0700

    updated

--- src/Template/functions-template.php
+++ src/Template/functions-template.php
@@ -1,11 +1,11 @@
-<?php //phpcs:ignore
+<?php
 /**
- * Backdrop Core ( functions-template.php )
+ * Backdrop Core ( src/Template/functions-template.php )
  *
- * @package     Backdrop Core
- * @copyright   Copyright (C) 2019-2020. Benjamin Lu
- * @license     GNU General PUblic License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
- * @author      Benjamin Lu ( https://benjlu.com )
+ * @package   Backdrop Core
+ * @copyright Copyright (C) 2019-2021. Benjamin Lu
+ * @license   GNU General PUblic License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author    Benjamin Lu ( https://getbenonit.com )
  */
 
 /**
@@ -25,45 +25,13 @@ function path( $file = '' ) {
 	$file = ltrim( $file, '/' );
 	$path = apply_filters( 'backdrop/template/path', 'resources/views' );
 
-	return $file ? $path . $file : $path;
+	return $file ? trailingslashit( $path ) . $file : trailingslashit( $path );
 }
 
 /**
- * Output get_template_part();
- * 
- * @since  2.0.0
- * @access public
- */
-function get_template_part( $slug, $name = '' ) {
-	$path = path();
-
-	do_action( "get_template_part_{$slug}", $slug, $name );
-
-	$templates = [];
-
-	if ( $name ) {
-		$templates[] = "{$path}/{$slug}-{$name}.php";
-		$templates[] = "{$path}/{$slug}/{$name}.php";
-	}
-
-	$templates[] = "{$path}/{$slug}.php";
-	$templates[] = "{$path}/{$slug}/{$slug}.php";
-
-	$templates = apply_filters( "backdrop/{$slug}/template_hierarchy", $templates, $name );
-	$template  = apply_filters( "backdrop/{$slug}/template", locate_template( $templates ), $name );
-
-	if ( $template ) {
-		include( $template ); // phpcs:ignore
-	}
-
-}
-
-/**
- * A better `locate_template()` function than what core WP provides. Note that
- * this function merely locates templates and does no loading. Use the core
- * `load_template()` function for actually loading the template.
+ * A better `locate_template()` function than what core WP provides. 
  *
- * @since  5.0.0
+ * @since  2.0.0
  * @access public
  * @param  array|string  $templates
  * @return string
@@ -71,7 +39,7 @@ function get_template_part( $slug, $name = '' ) {
 function locate( $templates ) {
 	$located = '';
 
-	foreach ( (array) $templates as $template ) {
+	foreach ( ( array ) $templates as $template ) {
 
 		foreach ( locations() as $location ) {
 
@@ -90,11 +58,7 @@ function locate( $templates ) {
 /**
  * Returns an array of locations to look for templates.
  *
- * Note that this won't work with the core WP template hierarchy due to an
- * issue that hasn't been addressed since 2010.
- *
- * @link   https://core.trac.wordpress.org/ticket/13239
- * @since  5.0.0
+ * @since  2.0.0
  * @access public
  * @return array
  */
@@ -110,5 +74,5 @@ function locations() {
 		$locations[] = get_template_directory() . "/{$path}";
 	}
 
-	return (array) apply_filters( 'hybrid/template/locations', $locations );
+	return ( array) apply_filters( 'backdrop/template/locations', $locations );
 }
