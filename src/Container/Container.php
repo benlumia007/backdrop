9cf9188aa69a56a6828b8a8b5927d022350c7fd4
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-11-03 13:27:22 -0700

    updated

--- src/Container/Container.php
+++ src/Container/Container.php
@@ -254,11 +254,6 @@ class Container implements ContainerContract, ArrayAccess {
 			return $this->instances[ $abstract ];
 		}
 
-		/**
-		 * Get the contextual concrete binding for the given abstract.
-		 */
-		$concrete = $this->getContextualConcrete( $abstract );
-
 		// Get the concrete implementation.
 		$concrete = $this->getConcrete( $abstract );
 
@@ -567,39 +562,4 @@ class Container implements ContainerContract, ArrayAccess {
 
 		return $this->factory( $name );
 	}
-
-    /**
-     * Get the contextual concrete binding for the given abstract.
-     *
-     * @param  string|callable  $abstract
-     * @return \Closure|string|array|null
-     */
-    protected function getContextualConcrete( $abstract ) {
-        if ( ! is_null( $binding = $this->findInContextualBindings( $abstract ) ) ) {
-            return $binding;
-        }
-
-        // Next we need to see if a contextual binding might be bound under an alias of the
-        // given abstract type. So, we will need to check if any aliases exist with this
-        // type and then spin through them and check for contextual bindings on these.
-        if ( empty( $this->abstractAliases[ $abstract ] ) ) {
-            return;
-        }
-
-        foreach ( $this->abstractAliases[ $abstract ] as $alias) {
-            if (! is_null( $binding = $this->findInContextualBindings( $alias ) ) ) {
-                return $binding;
-            }
-        }
-    }
-
-    /**
-     * Find the concrete binding for the given abstract in the contextual binding array.
-     *
-     * @param  string|callable  $abstract
-     * @return \Closure|string|null
-     */
-    protected function findInContextualBindings( $abstract ) {
-        return $this->contextual[ end( $this->buildStack ) ][ $abstract ] ?? null;
-    }
 }
