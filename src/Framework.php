9f4bc56ee09a8d9550c1975ed1a9be781200b8d1
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-01-26 19:13:14 -0800

    updated

--- src/Framework.php
+++ src/Framework.php
@@ -16,6 +16,7 @@ use Benlumia007\Backdrop\Proxies\Proxy;
 use Benlumia007\Backdrop\Proxies\App;
 
 use Benlumia007\Backdrop\Template\Hierarchy\Provider as HierarchyServiceProvider;
+use Benlumia007\Backdrop\Template\Manager\Provider as ManagerServiceProvider;
 use Benlumia007\Backdrop\View\View\Provider as ViewServiceProvider;
 
 /**
@@ -129,6 +130,7 @@ class Framework extends Container implements FrameworkContract, Bootable {
 			$this->provider( $provider );
 		}, [
 			HierarchyServiceProvider::class,
+			ManagerServiceProvider::class,
 			ViewServiceProvider::class,
 		] );
 	}
