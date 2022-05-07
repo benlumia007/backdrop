4c8ae4284d495aa7aa4c42a742fdf25a79c1f227
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-05-07 09:02:53 -0700

    updated

--- src/Framework.php
+++ src/Framework.php
@@ -17,6 +17,7 @@ use Benlumia007\Backdrop\Proxies\App;
 
 use Benlumia007\Backdrop\FontAwesome\Provider as FontAwesomeServiceProvider;
 use Benlumia007\Backdrop\GoogleFonts\Provider as GoogleFontsServiceProvider;
+use Benlumia007\Backdrop\Mix\Provider as MixServiceProvider;
 use Benlumia007\Backdrop\Template\Hierarchy\Provider as HierarchyServiceProvider;
 use Benlumia007\Backdrop\Template\Manager\Provider as ManagerServiceProvider;
 use Benlumia007\Backdrop\View\View\Provider as ViewServiceProvider;
@@ -135,6 +136,7 @@ class Framework extends Container implements FrameworkContract, Bootable {
 			GoogleFontsServiceProvider::class,
 			HierarchyServiceProvider::class,
 			ManagerServiceProvider::class,
+			MixServiceProvider::class,
 			ViewServiceProvider::class,
 		] );
 	}
---
+++ src/Mix/functions-mix.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Benlumia007\Backdrop\Mix;
+
+function asset( $path ) {
+
+	// Get the Laravel Mix manifest.
+	$manifest = App::resolve( 'backdrop/mix' );
+
+	// Make sure to trim any slashes from the front of the path.
+	$path = '/' . ltrim( $path, '/' );
+
+	if ( $manifest && isset( $manifest[ $path ] ) ) {
+		$path = $manifest[ $path ];
+	}
+
+	return get_theme_file_uri( 'public' . $path );
+}
