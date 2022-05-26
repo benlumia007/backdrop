6d399e849224cd456e3777848c77cb17d354f13c
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-05-26 11:33:54 -0700

    removed and add those stuff to another repos

--- src/Framework.php
+++ src/Framework.php
@@ -15,13 +15,6 @@ use Benlumia007\Backdrop\Contracts\Bootable;
 use Benlumia007\Backdrop\Proxies\Proxy;
 use Benlumia007\Backdrop\Proxies\App;
 
-use Benlumia007\Backdrop\FontAwesome\Provider as FontAwesomeServiceProvider;
-use Benlumia007\Backdrop\GoogleFonts\Provider as GoogleFontsServiceProvider;
-use Benlumia007\Backdrop\Mix\Provider as MixServiceProvider;
-use Benlumia007\Backdrop\Template\Hierarchy\Provider as HierarchyServiceProvider;
-use Benlumia007\Backdrop\Template\Manager\Provider as ManagerServiceProvider;
-use Benlumia007\Backdrop\View\View\Provider as ViewServiceProvider;
-
 /**
  * Application class.
  *
@@ -86,7 +79,6 @@ class Framework extends Container implements FrameworkContract, Bootable {
 	public function __construct() {
 		$this->registerDefaultBindings();
 		$this->registerDefaultProxies();
-		$this->registerDefaultProviders();
 	}
 
 	/**
@@ -126,19 +118,6 @@ class Framework extends Container implements FrameworkContract, Bootable {
 	 */
 	protected function registerDefaultProxies() {
 		$this->proxy( App::class, 'Benlumia007\Backdrop\App' );
-	}
-
-	protected function registerDefaultProviders() {
-		array_map( function( $provider ) {
-			$this->provider( $provider );
-		}, [
-			FontAwesomeServiceProvider::class,
-			GoogleFontsServiceProvider::class,
-			HierarchyServiceProvider::class,
-			ManagerServiceProvider::class,
-			MixServiceProvider::class,
-			ViewServiceProvider::class,
-		] );
 	}
 
 	/**
