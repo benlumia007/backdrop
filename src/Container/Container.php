f9b0ec9699e452d7882fd344feb71b720e05c014
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-01-04 22:39:22 -0800

    updated

--- src/Container/Container.php
+++ src/Container/Container.php
@@ -7,7 +7,7 @@
  *
  * @package   Backdrop
  * @author    Benjamin Lu <benlumia007@gmail.com>
- * @copyright Copyright (C) 2019-2023. Benjamin Lu
+ * @copyright 2019-2023. Benjamin Lu
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
  */
 
