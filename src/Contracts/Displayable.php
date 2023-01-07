b600b9e976529229228376240eff53be12af5f45
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-01-06 20:28:07 -0800

    initial commit

---
+++ src/Contracts/Displayable.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Displayable contract.
+ *
+ * Displayable classes should be implemented by a `display()` method.
+ * This method should output HTML strings to the screen.
+ *
+ * @package   Backdrop
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2019-2023. Benjamin Lu
+ * @link      https://github.com/benlumia007/backdrop
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+namespace Backdrop\Contracts;
+
+/**
+ * Displayable interface
+ *
+ * @since  1.0.0
+ * @access public
+ */
+interface Displayable {
+
+	/**
+	 * Prints the HTML string.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function  display() : void;
+}
