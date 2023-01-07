e3b306312974c94ffd3578fd37056283a105cf50
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-01-06 20:39:40 -0800

    this file has been moved to src/Contracts/Core

--- src/Contracts/Core/Container.php
+++ src/Contracts/Core/Container.php
@@ -12,7 +12,7 @@
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
  */
 
-namespace Backdrop\Contracts;
+namespace Backdrop\Contracts\Core;
 
 use Backdrop\Core\ServiceProvider;
 use Closure;
