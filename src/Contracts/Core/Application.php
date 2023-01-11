f5758cefa8782cde2f4e9106ebd5c3a1633c9a53
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-01-11 09:28:59 -0800

    add src/Contracts/Core/Application.php back to the framework

---
+++ src/Contracts/Core/Application.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Application contract.
+ *
+ * The Application class should be the primary class for working with and
+ * launching the app. It extends the `Container` contract.
+ *
+ * @package   Backdrop
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2019-2023. Benjamin Lu
+ * @link      https://github.com/benlumia007/backdrop
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+namespace Backdrop\Contracts\Core;
+
+use Backdrop\Contracts\Core\Container;
+
+interface Application extends Container {
+
+	/**
+	 * Adds a service provider. Developers can pass in an object or a fully-
+	 * qualified class name.
+	 *
+	 * @since  2.0.0
+	 * @access public
+	 * @param  string|object  $provider
+	 * @return void
+	 */
+	public function provider( $provider ): void;
+
+	/**
+	 * Adds a static proxy alias. Developers must pass in fully-qualified
+	 * class name and alias class name.
+	 *
+	 * @since  2.0.0
+	 * @access public
+	 * @param  string  $class_name
+	 * @param  string  $alias
+	 * @return void
+	 */
+	public function proxy( string $class_name, string $alias ): void;
+}
