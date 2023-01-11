61cf76aeeed93908837a7f7726c810fa2a83293c
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-01-11 14:01:25 -0800

    udpated

--- src/functions-helpers.php
+++ src/functions-helpers.php
@@ -22,31 +22,29 @@ if (!function_exists(__NAMESPACE__ . '\\booted')) {
 	 * booted. Use before launching a new application. If booted, reference
 	 * the `app()` instance directly.
 	 *
-	 * @return bool
 	 * @since  2.0.0
 	 * @access public
+	 * @return bool
 	 */
-	function booted(): bool
-	{
+	function booted(): bool {
 		return defined('BACKDROP_BOOTED') && true === BACKDROP_BOOTED;
 	}
 }
 
-if (!function_exists(__NAMESPACE__ . '\\app')) {
+if ( !function_exists(__NAMESPACE__ . '\\app' ) ) {
 	/**
 	 * The single instance of the app. Use this function for quickly working
 	 * with data.  Returns an instance of the `\Hybrid\Core\Application`
 	 * class. If the `$abstract` parameter is passed in, it'll resolve and
 	 * return the value from the container.
 	 *
+	 * @since  2.0.0
+	 * @access public
 	 * @param string $abstract
 	 * @param array $params
 	 * @return mixed
-	 * @since  2.0.0
-	 * @access public
 	 */
-	function app(string $abstract = '', array $params = [])
-	{
+	function app( string $abstract = '', array $params = [] ): mixed {
 
 		return App::resolve($abstract ?: 'app', $params);
 	}
