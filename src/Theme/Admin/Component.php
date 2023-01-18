e127885f42444affc47d5f0d387983fb5b82b377
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-01-17 16:02:02 -0800

    updated

---
+++ src/Theme/Admin/Component.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Add Menu Page
+ *
+ * @package   Backdrop
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2019-2023. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/benlumia007/backdrop-theme
+ */
+
+/**
+ * Define namespace
+ */
+namespace Backdrop\Theme\Admin;
+
+use Backdrop\Contracts\Bootable;
+
+
+class Component implements Bootable {
+	/**
+	 * menu()
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 */
+	public function menu() {}
+
+	/**
+	 * callback()
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 */
+	public function callback() {}
+
+	/**
+	 * tabs()
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 */
+	public function tabs() {}
+
+	/**
+	 * pages()
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 */
+	public function pages() {}
+
+	public function boot(): void {
+		add_action( 'admin_menu', array( $this, 'menu' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue' ), true, '1.0.0' );
+	}
+}
